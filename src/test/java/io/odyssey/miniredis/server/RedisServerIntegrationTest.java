56fe4806eb49f53b38a32041e7feb6a432d1e589
khanhdoa-2809 <khanhdoan21.work@gmail.com>
2026-09-04 22:10:04 +0700

    feat: remove old test

--- src/test/java/io/odyssey/miniredis/server/RedisServerIntegrationTest.java
+++ src/test/java/io/odyssey/miniredis/server/RedisServerIntegrationTest.java
@@ -23,46 +23,6 @@ public class RedisServerIntegrationTest {
     }
 
     @Test
-    void shouldEchoBytesBackToClient() throws IOException {
-        server = new RedisServer(new ServerConfig( "127.0.0.1", 0));
-        server.start();
-
-        var port = server.getLocalPort();
-
-        try (var client = new Socket("127.0.0.1", port)) {
-            var output = client.getOutputStream();
-            var input = client.getInputStream();
-            var request = "hello".getBytes(StandardCharsets.UTF_8);
-            output.write(request);
-            output.flush();
-
-            var response = input.readNBytes(request.length);
-            assertThat(response).isEqualTo(request);
-        }
-    }
-
-    @Test
-    void shouldServerMultipleClients() throws IOException {
-        server = new RedisServer(new ServerConfig( "127.0.0.1", 0));
-        server.start();
-
-        var port = server.getLocalPort();
-        try(var clientOne = new Socket("127.0.0.1", port);
-            var clientTwo = new Socket("127.0.0.1", port)) {
-            clientOne.getOutputStream().write("one".getBytes(StandardCharsets.UTF_8));
-            clientOne.getOutputStream().flush();
-
-            clientTwo.getOutputStream().write("two".getBytes(StandardCharsets.UTF_8));
-            clientTwo.getOutputStream().flush();
-
-            var first = clientOne.getInputStream().readNBytes(3);
-            var second = clientTwo.getInputStream().readNBytes(3);
-            assertThat(new String(first, StandardCharsets.UTF_8)).isEqualTo("one");
-            assertThat(new String(second, StandardCharsets.UTF_8)).isEqualTo("two");
-        }
-    }
-
-    @Test
     void shouldAcceptRespRequest() throws Exception {
         server = new RedisServer(new ServerConfig("127.0.0.1", 0));
         server.start();
